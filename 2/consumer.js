fa5a14ffeafe85dc43497a0edb23e5aedeb69ef1
adnanb59 <adnan_bhuiyan@hotmail.com>
2020-05-15 23:42:22 -0400

    Did part 2

--- 2/consumer.js
+++ 2/consumer.js
@@ -1,9 +1,21 @@
 const amqp = require("amqplib");
+const q = "task_queue";
 
 const receive = async () => {
     try {
         let conn = await amqp.connect(`amqp://${process.env.RABBIT_HOST}`);
         let channel = await conn.createChannel();
+        channel.assertQueue(q, {durable: true});
+        channel.prefetch(1);
+        console.log("[R] Waiting for messages in %s. To exit press CTRL+C", q);
+        channel.consume(q, msg => {
+            let secs = msg.content.toString().split(".").length - 1;
+            console.log("[R] Received ", msg.content.toString());
+            setTimeout(() => {
+                console.log("[R] Done.");
+                channel.ack(msg);
+            }, secs*1000);
+        }, {noAck: false});
     } catch (err) {
         throw err;
     }
--- 2/producer.js
+++ 2/producer.js
@@ -1,9 +1,19 @@
 const amqp = require("amqplib");
 
+const q = "task_queue";
+
 const send = async () => {
     try {
         let conn = await amqp.connect(`amqp://${process.env.RABBIT_HOST}`);
         let channel = await conn.createChannel();
+        let msg = process.argv.slice(2).join(" ") || "Hello World!";
+        channel.assertQueue(q, {durable: true});
+        channel.sendToQueue(q, Buffer.from(msg), {persistent: true});
+        console.log("[S] Sent ", msg);
+        setTimeout(() => {
+            conn.close();
+            process.exit(0);
+        }, 500);
     } catch (err) {
         throw err;
     }
